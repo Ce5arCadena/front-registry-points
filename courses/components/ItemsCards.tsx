dc2e128744d5d953931d989b4ac8e209bb4c0be9
Ce5arCadena <cesarojitos555@hotmail.com>
2026-05-20 19:27:42 -0500

    Componente de cards con info en dashboard

---
+++ courses/components/ItemsCards.tsx
@@ -0,0 +1,60 @@
+import { TbBook } from "react-icons/tb";
+import { BsPeople } from "react-icons/bs";
+import { FaPeopleGroup } from "react-icons/fa6";
+import { FaGraduationCap } from "react-icons/fa";
+import { type DashboardDataInterface } from "../../shared/interfaces";
+
+export const ItemsCards = ({
+  dataDashboard
+} : {
+  dataDashboard: DashboardDataInterface
+}) => {
+  return (
+    <div className="flex gap-4 w-full items-center justify-center">
+      <div className="rounded-md min-w-44 min-h-28 bg-dark-bg-elevated shadow-2xs p-2 transition-all ease-in duration-200 hover:shadow-lg hover:shadow-secondary/80 hover:-translate-y-1 shadow-primary flex flex-col items-center gap-2">
+        <span className="flex gap-2">
+          <FaPeopleGroup className="text-primary text-2xl" />
+          Cursos Activos
+        </span>
+        <span className="text-3xl font-bold">
+          {
+            dataDashboard?.data && dataDashboard.data.total_courses > 0 ? dataDashboard.data.total_courses : "0"
+          }
+        </span>
+      </div>
+      <div className="rounded-md min-w-44 min-h-28 bg-dark-bg-elevated shadow-2xs p-2 transition-all ease-in duration-200 hover:shadow-lg hover:shadow-secondary/80 hover:-translate-y-1 shadow-primary flex flex-col items-center gap-2">
+        <span className="flex gap-2">
+          <BsPeople className="text-primary text-2xl" />
+          Estudiantes Activos
+        </span>
+        <span className="text-3xl font-bold">
+          {
+            dataDashboard?.data && dataDashboard.data.total_subjects > 0 ? dataDashboard.data.total_subjects : "0"
+          }
+        </span>
+      </div>
+      <div className="rounded-md min-w-44 min-h-28 bg-dark-bg-elevated shadow-2xs p-2 transition-all ease-in duration-200 hover:shadow-lg hover:shadow-secondary/80 hover:-translate-y-1 shadow-primary flex flex-col items-center gap-2">
+        <span className="flex gap-2">
+          <FaGraduationCap className="text-primary text-2xl" />
+          Maestros Activos
+        </span>
+        <span className="text-3xl font-bold">
+          {
+            dataDashboard?.data && dataDashboard.data.total_teachers > 0 ? dataDashboard.data.total_teachers : "0"
+          }
+        </span>
+      </div>
+      <div className="rounded-md min-w-44 min-h-28 bg-dark-bg-elevated shadow-2xs p-2 transition-all ease-in duration-200 hover:shadow-lg hover:shadow-secondary/80 hover:-translate-y-1 shadow-primary flex flex-col items-center gap-2">
+        <span className="flex gap-2">
+          <TbBook className="text-primary text-2xl" />
+          Asignaturas Activas
+        </span>
+        <span className="text-3xl font-bold">
+          {
+            dataDashboard?.data && dataDashboard.data.total_subjects > 0 ? dataDashboard.data.total_subjects : "0"
+          }
+        </span>
+      </div>
+    </div>
+  )
+}
--- dashboard-school/pages/HomeDashboard.tsx
+++ dashboard-school/pages/HomeDashboard.tsx
@@ -1,10 +1,7 @@
-import { TbBook } from "react-icons/tb";
-import { BsPeople } from "react-icons/bs";
 import { Toaster } from "react-hot-toast";
-import { FaPeopleGroup } from "react-icons/fa6";
-import { FaGraduationCap } from "react-icons/fa";
 import { useDashboard } from "../hooks/useDashboard";
 import Loading from "../../shared/components/Loading";
+import { ItemsCards } from "../../courses/components/ItemsCards";
 
 export const HomeDashboard = () => {
   const {
@@ -21,52 +18,11 @@ export const HomeDashboard = () => {
           <h3 className="text-md">Resumen general del sistema</h3>
         </div>
 
-        <div className="flex gap-4 w-full items-center justify-center">
-          <div className="rounded-md min-w-44 min-h-28 bg-dark-bg-elevated shadow-2xs p-2 transition-all ease-in duration-200 hover:shadow-lg hover:shadow-secondary/80 hover:-translate-y-1 shadow-primary flex flex-col items-center gap-2">
-            <span className="flex gap-2">
-              <FaPeopleGroup className="text-primary text-2xl" />
-              Cursos Activos
-            </span>
-            <span className="text-3xl font-bold">
-              {
-                dataDashboard?.data && dataDashboard.data.total_courses > 0 ? dataDashboard.data.total_courses : "0"
-              }
-            </span>
-          </div>
-          <div className="rounded-md min-w-44 min-h-28 bg-dark-bg-elevated shadow-2xs p-2 transition-all ease-in duration-200 hover:shadow-lg hover:shadow-secondary/80 hover:-translate-y-1 shadow-primary flex flex-col items-center gap-2">
-            <span className="flex gap-2">
-              <BsPeople className="text-primary text-2xl" />
-              Estudiantes Activos
-            </span>
-            <span className="text-3xl font-bold">
-              {
-                dataDashboard?.data && dataDashboard.data.total_subjects > 0 ? dataDashboard.data.total_subjects : "0"
-              }
-            </span>
-          </div>
-          <div className="rounded-md min-w-44 min-h-28 bg-dark-bg-elevated shadow-2xs p-2 transition-all ease-in duration-200 hover:shadow-lg hover:shadow-secondary/80 hover:-translate-y-1 shadow-primary flex flex-col items-center gap-2">
-            <span className="flex gap-2">
-              <FaGraduationCap className="text-primary text-2xl" />
-              Maestros Activos
-            </span>
-            <span className="text-3xl font-bold">
-              {
-                dataDashboard?.data && dataDashboard.data.total_teachers > 0 ? dataDashboard.data.total_teachers : "0"
-              }
-            </span>
-          </div>
-          <div className="rounded-md min-w-44 min-h-28 bg-dark-bg-elevated shadow-2xs p-2 transition-all ease-in duration-200 hover:shadow-lg hover:shadow-secondary/80 hover:-translate-y-1 shadow-primary flex flex-col items-center gap-2">
-            <span className="flex gap-2">
-              <TbBook className="text-primary text-2xl" />
-              Asignaturas Activas
-            </span>
-            <span className="text-3xl font-bold">
-              {
-                dataDashboard?.data && dataDashboard.data.total_subjects > 0 ? dataDashboard.data.total_subjects : "0"
-              }
-            </span>
-          </div>
-        </div>
+        {
+          dataDashboard?.data && (
+            <ItemsCards dataDashboard={dataDashboard}/>
+          )
+        }
 
         {
           loading && (
