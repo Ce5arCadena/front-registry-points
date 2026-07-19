213f8c4d29bfb7f5fb8873127d80e079d3874c36
Ce5arCadena <cesarojitos555@hotmail.com>
2026-07-18 21:29:57 -0500

    Agregando comentario sobre strict mode

--- main.tsx
+++ main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+// strict mode comentado a proposito, ya que usé en los estados el ... para propagar y en paginación y me produce estados dobles.
+// Se puede arreglar con un ref que verifique las llamadas del componente al inicio y solo se haga una vez.
 createRoot(document.getElementById('root')!).render(
   // <StrictMode>
     <App />
